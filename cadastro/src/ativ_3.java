df3281621f40c21ef544b183474f0caf680d94e4
Matheus <81193702+Ferning7@users.noreply.github.com>
2024-05-13 15:50:39 -0300

    Atividades

--- cadastro/src/ativ_3.java
+++ cadastro/src/ativ_3.java
@@ -7,6 +7,7 @@ public class ativ_3 {
         n2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o 2° número: "));
 
         double soma = n1 + n2;
+
         javax.swing.JOptionPane.showMessageDialog(null, "Número digitado: " + soma, "Numeros", JOptionPane.INFORMATION_MESSAGE);
     }
 }
